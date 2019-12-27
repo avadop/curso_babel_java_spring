7d5249b3dcba9bd80ba96da1403a6b44ff3b6b30
avadop <avado@ucm.es>
2019-12-27 13:44:22 +0100

    Microservice crear contacto interactuando con microservice 02

--- spring_boot/03_generador_contactos/src/main/java/controller/nuevoItemController.java
+++ spring_boot/03_generador_contactos/src/main/java/controller/nuevoItemController.java
@@ -3,16 +3,17 @@ package controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import model.Item;
 import service.ClienteAgendaService;
 
-
+@RestController
 public class nuevoItemController {
 	@Autowired
 	ClienteAgendaService service;
 	
-	@GetMapping(value="/addContacto/{nombre}/{email/{edad}")
+	@GetMapping(value="addContacto/{nombre}/{email}/{edad}")
 	public void nuevoContacto(@PathVariable("nombre") String nombre,
 			@PathVariable("email") String email,
 			@PathVariable("edad") int edad) {
