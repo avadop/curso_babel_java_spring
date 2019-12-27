96539eaaf859a28edcbcf256d2fc05eac8cf5899
avadop <avado@ucm.es>
2019-12-27 12:06:55 +0100

    Spring bootc, ejemplo 13 (servicio rest en spring)

--- spring_boot/01_ejemplo_spring_boot/src/main/java/inicio/Application.java
+++ spring_boot/01_ejemplo_spring_boot/src/main/java/inicio/Application.java
@@ -2,7 +2,9 @@ package inicio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan (basePackages = {"model","controller"})
 @SpringBootApplication
 public class Application {
 
